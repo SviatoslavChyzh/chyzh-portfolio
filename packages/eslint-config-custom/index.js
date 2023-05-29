e1a882eb4ce5bd73e9ef918361c63fc5a277cb5a
Sviatoslav.Chyzh <sviatoslav.chyzh@gfk.com>
2023-05-29 16:24:07 +0200

    Configured eslint

---
+++ packages/eslint-config-custom/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "turbo",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+    rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "no-confusing-arrow": ["error", { "allowParens": false }],
+    "no-mixed-operators": "error",
+    "no-tabs": ["error", {"allowIndentationTabs": true}],
+    "react/react-in-jsx-scope": "off"
+  }
+};
