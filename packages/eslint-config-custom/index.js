2b46bcdf57139faaddc7be631adb23795e4f2e6c
Sviatoslav.Chyzh <sviatoslav.chyzh@gfk.com>
2023-05-31 23:17:36 +0200

    Configured prettier

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,23 +1,24 @@
+// eslint-disable-next-line no-undef
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "turbo",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'turbo',
+    'prettier',
   ],
-  plugins: ["@typescript-eslint"],
-  parser: "@typescript-eslint/parser",
+  plugins: ['@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
-    rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "no-confusing-arrow": ["error", { "allowParens": false }],
-    "no-mixed-operators": "error",
-    "no-tabs": ["error", {"allowIndentationTabs": true}],
-    "react/react-in-jsx-scope": "off"
-  }
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-confusing-arrow': ['error', { allowParens: false }],
+    'no-mixed-operators': 'error',
+    'no-tabs': ['error', { allowIndentationTabs: true }],
+    'react/react-in-jsx-scope': 'off',
+  },
 };
