c7d78aa7dcb1b1d93c60290a7c7f9b818bdc41f4
Sviatoslav.Chyzh <sviatoslav.chyzh@gfk.com>
2023-06-30 14:32:09 +0200

    Created portfolio CSS page

--- apps/portfolio/src/App.tsx
+++ apps/portfolio/src/App.tsx
@@ -1,31 +1,124 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
-import { Button } from 'ui';
+import 'ui/style.css';
+import { BsFiletypeCss, BsFiletypeHtml, BsFiletypeScss } from 'react-icons/bs';
+import { BiLogoJavascript, BiLogoReact, BiLogoTypescript } from 'react-icons/bi';
+import { useLocalStorage } from './shared/hooks/useLocalStorage';
+import { useQuery } from '@tanstack/react-query';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', prefersDark ? 'dark' : 'light');
+
+  const result = useQuery({
+    queryKey: ['test'],
+    queryFn: () =>
+      Promise.resolve([
+        { id: 1, name: 'test' },
+        { id: 2, name: 'test2' },
+        { id: 3, name: 'test3' },
+      ]),
+    staleTime: Infinity,
+  });
+
+  if (result.isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <form className={'color-picker'}>
+        <fieldset>
+          <legend className={'visually-hidden'}>Pick a color scheme</legend>
+          <label htmlFor="light" className={'visually-hidden'}>
+            Light
+          </label>
+          <input
+            type="radio"
+            name="theme"
+            id="light"
+            onChange={() => setTheme('light')}
+            checked={theme === 'light'}
+            value={theme}
+          />
+          <label htmlFor="dark" className={'visually-hidden'}>
+            Dark
+          </label>
+          <input
+            type="radio"
+            name="theme"
+            id="dark"
+            onChange={() => setTheme('dark')}
+            checked={theme === 'dark'}
+            value={theme}
+          />
+        </fieldset>
+      </form>
+      <div className={'grid-container'}>
+        <aside>
+          <ul className={'icon-list'} role="list">
+            <li>
+              <BsFiletypeHtml size={50} />
+            </li>
+            <li>
+              <BsFiletypeCss size={50} />
+            </li>
+            <li>
+              <BsFiletypeScss size={50} />
+            </li>
+            <li>
+              <BiLogoJavascript size={50} />
+            </li>
+            <li>
+              <BiLogoTypescript size={50} />
+            </li>
+            <li>
+              <BiLogoReact size={50} />
+            </li>
+          </ul>
+        </aside>
+        <main>
+          <p className={'eyebrow'}>skills & experience</p>
+          <h1 className={'page-title'}>My Resume</h1>
+          <p className={'intro'}>Intro</p>
+          <div className={'job-experience'}>
+            <div className={'job-experience__item'}>
+              <h2 className={'job-experience__year'}>Job Year</h2>
+              <div className={'job-experience__details'}>
+                <h2 className={'job-experience__role'}>Job Role</h2>
+                <span className={'job-experience__company'}>Company name</span>
+              </div>
+              <p>Description</p>
+            </div>
+
+            <div className={'job-experience__item'}>
+              <h2 className={'job-experience__year'}>Job Year</h2>
+              <div className={'job-experience__details'}>
+                <h2 className={'job-experience__role'}>Job Role</h2>
+                <span className={'job-experience__company'}>Company name</span>
+              </div>
+              <p>Description</p>
+            </div>
+
+            <div className={'job-experience__item'}>
+              <h2 className={'job-experience__year'}>Job Year</h2>
+              <div className={'job-experience__details'}>
+                <h2 className={'job-experience__role'}>Job Role</h2>
+                <span className={'job-experience__company'}>Company name</span>
+              </div>
+              <p>Description</p>
+            </div>
+
+            <div className={'job-experience__item'}>
+              <h2 className={'job-experience__year'}>Job Year</h2>
+              <div className={'job-experience__details'}>
+                <h2 className={'job-experience__role'}>Job Role</h2>
+                <span className={'job-experience__company'}>Company name</span>
+              </div>
+              <p>Description</p>
+            </div>
+          </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <Button>Click me</Button>
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
 }
--- apps/portfolio/src/main.tsx
+++ apps/portfolio/src/main.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ToastContainer />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
+    </Router>
   </React.StrictMode>
 );
