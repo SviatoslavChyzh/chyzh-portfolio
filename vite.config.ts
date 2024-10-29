d0f634eded42e3eb48f8c62f72dbe5c0ec7f5fa2
Sviatoslav.Chyzh <sviatoslav.chyzh@gfk.com>
2024-10-29 11:42:00 +0100

    Initialize web application via create-cloudflare CLI
    Details:
      C3 = create-cloudflare@2.30.1
      project name = chyzh-portfolio
      framework = remix
      framework cli = create-remix@2.13.1
      package manager = bun@1.1.31
      wrangler = wrangler@3.82.0
      git = 2.39.2 (Apple Git-143)

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,28 @@
-import path from 'path';
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import {
+  vitePlugin as remix,
+  cloudflareDevProxyVitePlugin as remixCloudflareDevProxy,
+} from "@remix-run/dev";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+declare module "@remix-run/cloudflare" {
+  interface Future {
+    v3_singleFetch: true;
+  }
+}
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [
+    remixCloudflareDevProxy(),
+    remix({
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+        v3_singleFetch: true,
+        v3_lazyRouteDiscovery: true,
+      },
+    }),
+    tsconfigPaths(),
+  ],
 });
